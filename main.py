bf30d889733d7d445dd3d6289851144f9b47ecd8
Sudhanshu0007 <dwivedisudhanshu0@gmail.com>
2024-05-05 02:16:22 +0530

    "Learning basics of api creation and html request methods"

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
+from typing import Optional
 from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel 
 
 app = FastAPI()
+
+
+class post(BaseModel):
+    title: str
+    content: str
+    published:bool=True
+    rating: Optional[int]=None
+
 
 @app.get("/") 
 def root():
@@ -10,3 +21,9 @@ def root():
 @app.get("/posts")
 def get_post():
     return {"data": "This is your post"}
+
+@app.post("/createposts")
+def create_post(post:post):
+    print(post)
+    print(post.dict())
+    return {"data":"post"}
